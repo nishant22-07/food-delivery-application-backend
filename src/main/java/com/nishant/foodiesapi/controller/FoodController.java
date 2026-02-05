c0ed32a431c48980831843d9a67bf10a6fd41fec
nishant <nishantkudale04@gmail.com>
2026-02-05 13:26:51 +0530

    implement CRUD operations in FoodController

--- src/main/java/com/nishant/foodiesapi/controller/FoodController.java
+++ src/main/java/com/nishant/foodiesapi/controller/FoodController.java
@@ -1,8 +1,66 @@
 package com.nishant.foodiesapi.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nishant.foodiesapi.IO.FoodRequest;
+import com.nishant.foodiesapi.IO.FoodResponse;
+import com.nishant.foodiesapi.service.FoodService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/foods")
+@AllArgsConstructor
 public class FoodController {
+
+    private FoodService foodService;
+
+    @PostMapping
+    public FoodResponse addFood(@RequestPart("food") String foodString, @RequestPart("file") MultipartFile file){
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        FoodRequest request = null;
+        try{
+            request = objectMapper.readValue(foodString, FoodRequest.class);
+
+        }catch(JsonProcessingException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        FoodResponse foodResponse = foodService.addFood(request,file);
+        return foodResponse;
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods(){
+        return foodService.readFoods();
+    }
+
+    @GetMapping("/{id}")
+    public FoodResponse readFoodById(@PathVariable String id){
+        return foodService.readFoodById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFoodById(@PathVariable String id){
+        foodService.deleteFood(id);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
