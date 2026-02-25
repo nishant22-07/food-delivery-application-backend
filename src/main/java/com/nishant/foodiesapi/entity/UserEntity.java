ba0985e2eb2eceaaac81d12ba27afa6a44f00b60
nishant <nishantkudale04@gmail.com>
2026-02-25 13:43:20 +0530

    User Entity

--- src/main/java/com/nishant/foodiesapi/entity/UserEntity.java
+++ src/main/java/com/nishant/foodiesapi/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package com.nishant.foodiesapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,10 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "users")
 @Builder
 public class UserEntity {
+
     @Id
     private String id;
+
     private String name;
     private String email;
     private String password;
-
 }
