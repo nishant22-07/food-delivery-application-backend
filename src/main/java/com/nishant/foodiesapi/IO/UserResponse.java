fe8c82395b0cd316bd78673019beaf49dad5f561
nishant <nishantkudale04@gmail.com>
2026-02-25 13:41:07 +0530

    User IO's

---
+++ src/main/java/com/nishant/foodiesapi/IO/UserResponse.java
@@ -0,0 +1,20 @@
+package com.nishant.foodiesapi.IO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+}
